024c7a8166f8851cd31414b30037a9c2c7d41b82
Nicolas Fréret <contact.nicolasfreret@gmail.com>
2025-03-20 12:05:46 +0100

    add emaild

--- email.php
+++ email.php
@@ -1,8 +1,29 @@
 <?php
 
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+
+
+
+$message = '
+  <p>Prenom : '.$_POST['fname'].'</p>
+  <p>Nom : '.$_POST['lname'].'</p>
+  <p>Téléphone : '.$_POST['phone'].'</p>
+  <p>Email : '.$_POST['email'].'</p>
+  <p>Status : '.$_POST['job'].'</p>
+  <p>'.$_POST['msg'].'</p>
+';
+
+$m = file_get_contents('t.html');
+
+$m = str_replace($_POST['fname'],"{nom}",$m);
 
 mail(
-    'contact.nicolasfreret@gmail.com',
-    'Test',
-    'Coucou, tu es un gars super, ne loublie jamais !'
+    'tonemail@gmail.com',
+    'Message du site',
+    $m,
+    $headers
 );
+
+exit('mail envoyé');
